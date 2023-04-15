7119bf9dd8f47f47c2bf2e38c4735e7f7d231b55
DimonPerminov <perminov.dima1@mail.ru>
2023-04-15 20:30:47 +0500

    Declared initial classes

--- Gladiators (Task 5).cpp
+++ Gladiators (Task 5).cpp
@@ -3,6 +3,49 @@
 
 using namespace std;
 
+class Gladiator
+{
+
+};
+
+class Weapon
+{
+
+};
+
+class Defence
+{
+
+};
+
+class Sword : public Weapon
+{
+
+};
+
+class Spear : public Weapon
+{
+
+};
+
+class Helmet : public Defence
+{
+
+};
+
+class ChestPlate : public Defence
+{
+	
+};
+
+class Greaves : public Defence
+{
+
+};
+
+
+
+
 int main() {
 
 }
